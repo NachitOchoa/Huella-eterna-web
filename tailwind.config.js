9cad946e72deee5a9d22ca7505d47a42aedcd1cc
NachitOchoa <psicomarochoa@gmail.com>
2025-05-21 09:18:12 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        beige: '#F9F5EC',
-        pastelBlue: '#A3C9D9'
+        beige: "#F9F5EC",
+        pastelBlue: "#A3C9D9",
+        olive: "#7B9E89",
+        golden: "#D4A373",
       },
-      fontFamily: {
-        serif: ['Georgia', 'serif']
-      }
+    },
+    fontFamily: {
+      serif: ["Georgia", "serif"],
     },
   },
   plugins: [],
-}
+};
